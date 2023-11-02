836a3c5e3a7eb2110df30fb4d5b2165419bfdbac
masmuss <ahmusafir.khoirul@gmail.com>
2023-11-02 19:46:43 +0700

    fix(reponsive): fix footer font size
    Signed-off-by: masmuss <ahmusafir.khoirul@gmail.com>

--- components/partials/footer.tsx
+++ components/partials/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
 	return (
 		<footer className="static bottom-0 left-0 w-full py-6 text-center text-zinc-600 dark:text-zinc-400">
 			<div className="container">
-				<p className="text-sm">
+				<p className="text-xs md:text-sm">
 					made with ❤️ by <span className="font-semibold">RXBCH</span>{' '}
 					in Surabaya, Indonesia
 				</p>
